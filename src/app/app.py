1f00eb620b838bb448ded57427b029c20555f5d1
deadaf <gahlyanrohit18@gmail.com>
2023-01-22 01:05:05 +0530

    remove txns model & wallet routes

--- src/app/app.py
+++ src/app/app.py
@@ -5,9 +5,8 @@ from fastapi.security import APIKeyHeader
 from decouple import config
 
 from .routes._image import router as _image_router
-from .routes._wallet import router as _wallet_router
 
-from tortoise.contrib.fastapi import register_tortoise
+
 import os
 
 api_scheme = APIKeyHeader(name="authorization")
@@ -28,10 +27,3 @@ async def root():
 
 
 app.include_router(_image_router)
-app.include_router(_wallet_router)
-
-# register_tortoise(
-#     app,
-#     db_url="sqlite://:memory:",
-#     modules={"models": ["models"]},
-# )

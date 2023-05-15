cdbd140aa35388df5dc239fe3440ae2f90ee6f80
Rohit Kumar <gahlyanrohit18@gmail.com>
2023-05-15 23:47:16 +0530

    add support for a temp secret key

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,5 +6,5 @@ api_scheme = APIKeyHeader(name="authorization")
 
 
 async def verify_key(key: str = Depends(api_scheme)):
-    if key != config("FASTAPI_KEY"):
+    if key not in [config("FASTAPI_KEY"), config("TEMP_KEY")]:
         raise HTTPException(status_code=403, detail="Invalid API key")

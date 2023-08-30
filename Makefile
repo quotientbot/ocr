38572f76d1efe4fe5c7207f4990e44594977e106
deadaf <gahlyanrohit18@gmail.com>
2023-08-31 05:19:59 +0530

    setup local test env

--- Makefile
+++ Makefile
@@ -8,5 +8,8 @@ tidy:
 build:
 	env CGO_ENABLED=0 GOOS=linux GOARCH=amd64  go build  -o bin/ocr .
 
+test: tidy build
+	sls invoke local -f ocr --path event.json
+	
 deploy: tidy build
 	sls deploy --verbose
--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"context"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response events.APIGatewayProxyResponse
+type Response struct {
+	Body       string `json:"body"`
+	StatusCode int    `json:"statusCode"`
+}
+type Request struct {
+	Image string `json:"image"`
+}
 
 func generateResponse(Body string, Code int) Response {
 	return Response{Body: Body, StatusCode: Code}
 }
-func handleRequest(_ context.Context, request events.LambdaFunctionURLRequest) (Response, error) {
-	return generateResponse("Hello World!", 200), nil
+
+func handleRequest(_ context.Context, request Request) (Response, error) {
+	return generateResponse(request.Image, 200), nil
 }
 func main() {
 	lambda.Start(handleRequest)
